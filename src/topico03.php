857f368f6798b58f29254e940e40fa546600aecb
CarolinaZanardi9 <c.zanardi@uni9.edu.br>
2026-09-10 00:42:42 +0000

    topico03

---
+++ src/topico03.php
@@ -0,0 +1,15 @@
+<?php
+$cep="01012123";
+echo "cep = $cep";
+$valor=5124.784;
+echo "<br> Valor = $valor";
+echo "<br>R$ ".number_format($valor,2,",",".");
+echo "<br>R$ ".number_format($valor,0,",",".");
+
+$nome= "bete";
+echo"<br> Nome = $nome";
+echo'<br> Nome = $nome';
+$cor="green";
+echo"<p style='color: $cor'>$nome</p>";
+
+?>
